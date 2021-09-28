a89dbb3db46b1b4fa07593332d2ba04caca5ac3f
OrionRoven <oroven30@stuy.edu>
2021-09-28 19:54:58 -0400

    update Time.java

--- pre/02/Time.java
+++ pre/02/Time.java
@@ -4,19 +4,29 @@ public class Time{
           int minute;
           int second;
           int secsScince12;
+          int newSecsScince12;
           int secsLeft;
           double percentDay;
+          int elapsedTime;
 
-          hour=20;
-          minute=31;
+          hour=19;
+          minute=10;
           second=22;
           secsScince12=second+(60*minute)+(3600*hour);
           secsLeft=86400-secsScince12;
-          percentDay=secsScince12/86400;
+          percentDay=secsScince12/86400.0;
 
           System.out.println(secsScince12); //seconds since midnight
           System.out.println(secsLeft); //seconds left in day
-          System.out.println("Percentage of day passed: "+(percentDay*100)+"%"); //% day passed
+          System.out.println((percentDay*100)+"%"); //% day passed
+
+          hour=19;
+          minute=58;
+          second=44;
+          newSecsScince12=second+(60*minute)+(3600*hour);
+          elapsedTime=newSecsScince12-secsScince12;
+
+          System.out.println(elapsedTime+" seconds elapsed");
 
       }
     }
