99cd59cf343e689c0cdf43b845a8badeca6c1ffd
OrionRoven <oroven30@stuy.edu>
2021-09-30 17:36:57 -0400

    add discoveries to Greet.java in HW08

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,7 +1,16 @@
-//Augment main() to greet 3 hardcoded persons of interest.
-//What does this mean?
+/*Orion Roven
+APCS
+HW08 08_helloclass
+9-30-21
 
-   
+discoveries:
+	java requires you to assign a variable to a string.
+	If you say greet(String), it returns an error
+	If you say greet(String x), then x represents the inputted string and no errors happen
+
+My program works, but I don't really understand why because I called the function greet before I defined it.
+Does java not work like other languages in this way?
+*/
 public class Greet {
 	public static void main(String[] args) {
 		greet("Moe");
