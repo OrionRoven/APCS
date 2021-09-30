17342ab25cfd1ed3465506e88bb360c22594a667
OrionRoven <oroven30@stuy.edu>
2021-09-30 13:19:13 -0400

    init commit

---
+++ 08_hellofxn/greet.java
@@ -0,0 +1,9 @@
+public class greet{
+	public static void main(String[]args){
+		System.out.println(greet("no hablo queso"));
+		//System.out.println(greet("Moo"));
+	}
+	public static void greet(String x){
+		System.out.println(x);
+	}
+}
