064f1d8fc9f211b4afd888fb7562771066dbce03
OrionRoven <oroven30@stuy.edu>
2021-10-01 13:12:55 -0400

    add BigSib.java

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,14 @@
+/*
+Orion Roven
+APCS
+HW09 09_bigsib (greet all the ducks)
+10/1/21
+*/
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Von Geburt");//my duck
+        BigSib.greet("Versa");//max's duck
+        BigSib.greet("Soup");//paul's duck
+    }
+}
