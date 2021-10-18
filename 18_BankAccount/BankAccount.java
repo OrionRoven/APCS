1b753cb42849e01634fb56478158eea23361694c
OrionRoven <oroven30@stuy.edu>
2021-10-17 20:35:17 -0400

    update 18_BankAccount

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,3 +1,4 @@
+
 /*
 Trio number int x (Orion Roven, May Qiu, Lawrence Joa)
 APCS
@@ -8,7 +9,6 @@ main must be static
 in order to maintain proper pluraity of the word dollar, we can use an if else statement that checks if bal==1
 if we say if (bal==1){//do stuff} then we get an error saying nonstatic variable bal cannot be referenced in a static context
 we can fix this by saying if (myDuck.bal==1){//do stuff}, because myDuck.bal is static
-
 QCC:
 if main has to reference SetAcct, and SetAcct has to reference acctHolder, than wouldn't acctHolder have to be a static instance variable (it works if it is)
 */
@@ -20,43 +20,40 @@ public class BankAccount {
 	private int accountNum;
 	private double bal;
 
-	public void SetAcctHolder(String name){
+	public void SetAcctInfo(String name, String pw, int PinInput, int acctNum, double startBal){
 		acctHolder=name;
-	}
-
-	public void SetPassword(String pw){
 		password=pw;
-	}
-
-	public void SetPin(int PinInput){
 		pin=PinInput;
-	}
-
-	public void SetAcctNum(int acctNum){
 		accountNum=acctNum;
-	}
-
-	public void SetBal(double startBal){
 		bal=startBal;
 	}
 
+	public void DepositMoney(double paycheck){
+		bal= bal + paycheck;
+	}
+
+	public void WithdrawMoney(double moneySpent){
+		bal= bal - moneySpent;
+	}
+
+	public static void PrintInfo(String accttholder, String password, int pin, int accountNum, int bal, double paycheck, double moneySpent){
+				BankAccount myDuck = new BankAccount();
+
+				myDuck.SetAcctInfo("Von Geburt","nunyazbizniz",3217,448921,1);
+				myDuck.DepositMoney(.05);
+				myDuck.WithdrawMoney(.5);
+
+				System.out.println("account holder: "+myDuck.acctHolder);
+				System.out.println("password: "+myDuck.password);
+				System.out.println("pin: "+myDuck.pin);
+				System.out.println("account number: " +myDuck.accountNum);
+				if (myDuck.bal==1) {
+					System.out.println("balance: "+myDuck.bal+" dollar");
+				} else {
+					System.out.println("balance: "+myDuck.bal+ " dollars");
+				}
+	}
 	public static void main(String[] args){
-		BankAccount myDuck = new BankAccount();
-
-		myDuck.SetAcctHolder("Von Geburt");
-		myDuck.SetPassword("nunyazbizniz");
-		myDuck.SetPin(3217);
-		myDuck.SetAcctNum(448921);
-		myDuck.SetBal(1);
-
-		System.out.println("account holder: "+myDuck.acctHolder);
-		System.out.println("password: "+myDuck.password);
-		System.out.println("pin: "+myDuck.pin);
-		System.out.println("account number: "+myDuck.accountNum);
-		if (myDuck.bal==1) {
-			System.out.println("balance: "+myDuck.bal+" dollar");
-		} else {
-			System.out.println("balance: "+myDuck.bal+" dollars");
-		}
+		PrintInfo("Von Geburt", "nunyazbizniz", 3217, 448921, 1, .05, .5);
 	}
 }
