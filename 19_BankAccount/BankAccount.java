65abf45ac708f007fc806790aaeb39277ff95660
OrionRoven <oroven30@stuy.edu>
2021-10-18 17:48:59 -0400

    update 19_BankAccount

--- 19_BankAccount/BankAccount.java
+++ 19_BankAccount/BankAccount.java
@@ -1,21 +1,15 @@
 /*
 Trio number int x (Orion Roven(Von Geburt), May Qiu(Perry Jr.), Lawrence Joa(Steve, John))
 APCS
-HW19 -- CPA-One / BankAccount.java code
+HW19 -- Mo Money Mo Problems ...MORE AWESOME
 10/18/21
 
 DISCO:
-main must be static
-in order to maintain proper pluraity of the word dollar, we can use an if else statement that checks if bal==1
-if we say if (bal==1){//do stuff} then we get an error saying nonstatic variable bal cannot be referenced in a static context
-we can fix this by saying if (myDuck.bal==1){//do stuff}, because myDuck.bal is static
-QCC:
-if main has to reference SetAcct, and SetAcct has to reference acctHolder, than wouldn't acctHolder have to be a static instance variable (it works if it is)
+|| is used in boolean statements to represent the word "or"
+e.g. if(x||y){//do stuff} means that the conditional will only happen if x is true or y is true.
 
-Q2: Before we create a default constructor and use BankAccount myDuck = new BankAccount();, we are creating an object myDuck in class BankAccount().
-Because we can make an instance variable with no arguments we know that there is a default constructor.
-Q3: Assuming that a String representation of an object means (String object), then we could use System.out.println(String myDuck), because myDuck is an object.
-System.out.println(myDuck.<any instance var that is a string>);
+QCC:
+our withdraw method and the one in the library repo are void (meaning that it doesn't return anything), so how can we return True or False if the method doesn't have a boolean return type?
 */
 
 public class BankAccount {
@@ -35,6 +29,10 @@ public class BankAccount {
    		     accountNum = 999999999;
    		     System.out.println("Must be 9 digit account number");
         }
+        if(pin<1000 || pin>9998){
+          pin=9999;
+          System.out.println("Must be 4 digit pin");
+        }
     }
 
     public void DepositMoney(double paycheck){
@@ -42,13 +40,16 @@ public class BankAccount {
     }
 
     public void WithdrawMoney(double moneySpent){
+        if (bal<moneySpent){
+          System.out.println("you are broke");
+        }
         bal= bal - moneySpent;
     }
 
     public static void PrintInfo(String accttholder, String password, int pin, int accountNum, int bal, double paycheck, double moneySpent){
                 BankAccount myDuck = new BankAccount();
 
-                myDuck.SetAcctInfo("Von Geburt","nunyazbizniz",3217,44892239,1);
+                myDuck.SetAcctInfo("Von Geburt","nunyazbizniz",3217,448922391,1);
                 myDuck.DepositMoney(.05);
                 myDuck.WithdrawMoney(.5);
 
@@ -62,7 +63,8 @@ public class BankAccount {
                     System.out.println("balance: "+myDuck.bal+ " dollars");
                 }
     }
+    
     public static void main(String[] args){
-        PrintInfo("Von Geburt", "nunyazbizniz", 3217, 44892239, 1, .05, .5);
+        PrintInfo("Von Geburt", "nunyazbizniz", 3217, 448922391, 1, .05, .5);
     }
 }
