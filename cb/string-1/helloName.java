27788b9ee72ab4f89031a34278e145c798637e6d
OrionRoven <oroven30@stuy.edu>
2021-10-30 14:04:04 -0400

    add main functions to test string-1 stuff

--- cb/string-1/helloName.java
+++ cb/string-1/helloName.java
@@ -2,4 +2,7 @@ public class helloName {
   public String helloName(String name) {
     return "Hello "+name+"!";
   }
+  public static void main(String[] args) {
+    System.out.println(helloName("Bob"));
+  }
 }
--- cb/string-1/makeAbba.java
+++ cb/string-1/makeAbba.java
@@ -2,4 +2,8 @@ public class makeAbba {
   public String makeAbba(String a, String b) {
     return a+b+b+a;
   }
+
+  public static void main(String[] args) {
+    System.out.println(makeAbba("Hi", "Bye"));
+  }
 }
