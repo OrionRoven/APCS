ba86d56acb5566c20e0b05738288089f200386b8
OrionRoven <oroven30@stuy.edu>
2021-11-05 18:38:50 -0400

    test pre-increment operators

--- 06_paradigms/hello.py
+++ 06_paradigms/hello.py
@@ -1,3 +1,7 @@
-print(addOne(2))
-def addOne(x):
-    return x+1
+def addOne(y):
+    return y+1
+def getBig(x):
+    while x!=:
+        x+=1
+        print(x)
+print(getBig(2))
---
+++ TestingThings/Increment.java
@@ -0,0 +1,11 @@
+public class Increment {
+  public static void main (String[] args) {
+    int i = 0;
+
+    System.out.println(i++);
+
+    i=0;
+
+    System.out.println(++i);
+  }
+}
