318d443bf7d45b1d40d6ed48501d212cf58a1101
OrionRoven <oroven30@stuy.edu>
2021-12-04 22:51:58 -0500

    add centeredAverage.java in array-2

--- cb/array-2/centeredAverage.java
+++ cb/array-2/centeredAverage.java
@@ -1,20 +1,29 @@
 /*
-Return the "centered" average of an array of ints, which we'll say is the mean average of the values,
-except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value,
-ignore just one copy, and likewise for the largest value. Use int division to produce the final average.
+Return the "centered" average of an array of ints,
+which we'll say is the mean average of the values,
+except ignoring the largest and smallest values in the array.
+If there are multiple copies of the smallest value,
+ignore just one copy, and likewise for the largest value.
+Use int division to produce the final average.
 You may assume that the array is length 3 or more.
 */
 public class centeredAverage {
   public static int centeredAverage(int[] nums) {
-    int average = 0;
-    int max = 0;
-    int min = 100;
-    int minCtr = 0;
-    int maxCtr = 0;
+    int avg=0;
+    int max=nums[0];
+    int min=nums[0];
     for(int i=0; i<nums.length; i++) {
       max = Math.max(nums[i], max);
       min = Math.min(nums[i], min);
+      avg+=nums[i];
     }
-    for()
+    avg-=max;
+    avg-=min;
+    return avg / (nums.length-2);
+  }
+  public static void main(String[] args) {
+    System.out.println(centeredAverage(new int[] {1, 2, 3, 4, 100}));
+    System.out.println(centeredAverage(new int[] {1, 1, 5, 5, 10, 8, 7}));
+    System.out.println(centeredAverage(new int[] {-10, -4, -2, -4, -2, 0}));
   }
 }
