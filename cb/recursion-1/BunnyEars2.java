03a0ef9404d6801d593ba9a4ee99cd2f7e75b2d9
OrionRoven <oroven30@stuy.edu>
2022-02-14 17:55:35 -0500

    bunnyears2

---
+++ cb/recursion-1/BunnyEars2.java
@@ -0,0 +1,22 @@
+// Standard Valentine Configuration: Orion Roven, Max Schneider
+// APCS period 8
+// 2022-2-15
+// time elapsed: _hrs
+
+public class BunnyEars {
+  public int bunnyEars2(int bunnies) {
+    if (bunnies == 0) {
+      return 0;
+    }
+    if (bunnies % 2 == 1) {
+      return 2 + bunnyEars2(bunnies-1);
+    }
+    return 3+ bunnyEars2(bunnies-1);
+  }
+  public static void main(String[] args) {
+    System.out.println(bunnyEars2(0));
+    System.out.println(bunnyEars2(1));
+    System.out.println(bunnyEars2(2));
+
+  }
+}
