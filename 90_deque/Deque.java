a91ee10e54d0bc2131702ba2835e4ff35df0847a
OrionRoven <oroven30@stuy.edu>
2022-04-13 13:11:09 -0400

    90

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -8,17 +8,34 @@ time spent: 1.0 hrs
 
 public interface Deque<T> {
 
+  //push methods
   public void addFirst(T x);
   public void addLast(T x);
+  //public boolean offerFirst(T x);
+  //public boolean offerLast(T x);
 
+  //remove methods
   public T removeFirst();
   public T removeLast();
+  //public T pollFirst();
+  //public T pollLast();
+  //public boolean removeFirstOccurrence();
+  //public boolean removeLastOccurrence();
 
+  //peek methods
   public T peekFirst();
   public T peekLast();
+  //public T getFirst();
+  //public T getLast();
 
+  //stack?
+  //public T pop();
+  //public void push(T x);
+
+  //miscellaneous
   public int size();
-
   public boolean isEmpty();
-
+  //public boolean contains();
+  //public Iterator<T> iterator();
+  //public Iterator<T> descendingIterator();
 }
